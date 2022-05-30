a6b759f3571598603a86e5e4f99da8969b7b9713
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-05-31 00:22:15 +0530

    wip: api testing

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true})
+const uri =
+  "mongodb+srv://ayushsoni1010:remote1010@cluster0.lovmb.mongodb.net/?retryWrites=true";
+
+mongoose.connect(uri, { useNewUrlParser: true });
 
 const connection = mongoose.connection;
-connection.once('open',()=>{
-  console.log("MongoDb database connection established successfully")
-})
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
