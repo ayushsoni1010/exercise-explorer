e98703589be1ebbf8a0169fcb7c624abcea6289d
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-06-07 00:06:55 +0530

    update: database connectivity and api testing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,6 +8,7 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/add").post((req, res) => {
+  // console.log("data is " + req);
   const username = req.body.username;
 
   const newUser = new User({ username });
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const uri = process.env.ATLAS_URI;
 
