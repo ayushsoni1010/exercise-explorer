4ee5e10eb7d44cb28a3e3637ebf0fedaa0804db0
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-06-18 17:41:07 +0530

    fix: uri issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
 
-require("dotenv").config();
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,9 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const uri = process.env.ATLAS_URI;
-
-mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+});
 
 const connection = mongoose.connection;
 
