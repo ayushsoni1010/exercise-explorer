4e11443f783ba5834a38d934ab6d9dd6e02097ad
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-06-19 18:39:24 +0530

    enhance: server.js and heroku deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
-require("dotenv").config();
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,9 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const uri = process.env.ATLAS_URI;
-
-mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+});
 
 const connection = mongoose.connection;
 
@@ -26,6 +27,14 @@ const usersRouter = require("./routes/users");
 
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
